2e15482421678540ab926258c0217473de78b958
kev0330 <33414005+kev0330@users.noreply.github.com>
2019-04-17 03:27:26 +0800

    Update MarketFactory.java

--- Assignments/src/main/java/cput/ac/za/factory/MarketFactory.java
+++ Assignments/src/main/java/cput/ac/za/factory/MarketFactory.java
@@ -5,75 +5,9 @@ import cput.ac.za.repository.marketIDGenerate;
 
 public class MarketFactory {
     
-    public static Market getMarket(String marketID) {
-        return new Market.Builder().marketID(marketIDGenerate.generateId())
-                .marketID(marketID)
+    public static Market getMarketNum(int marketNum) {
+        return new Market.Builder().marketNum(marketNum)
                 .build();
     }
-    
-    public static Customer getCustomer(String customerID) {
-        return new Customer.Builder().customerID(marketIDGenerate.generateId())
-                .customerID(customerID)
-                .build();
-    }
-    
-    public static Employee getEmployee(String employeeID) {
-        return new Employee.Builder().employeeID(marketIDGenerate.generateId())
-                .employeeID(employeeID)
-                .build();
-    }
-    
-    public static Item getItem(String itemNum) {
-        return new Item.Builder().itemNum(marketIDGenerate.generateId())
-                .itemNum(itemNum)
-                .build();
-    }
-    
-    public static MarketOwner getMarketOwner(String marketOwnerID) {
-        return new MarketOwner.Builder().marketOwnerID(marketIDGenerate.generateId())
-                .marketOwnerID(marketOwnerID)
-                .build();
-    }
-    
-    public static Orders getOrders(String orderNum) {
-        return new Orders.Builder().orderNum(marketIDGenerate.generateId())
-                .orderNum(orderNum)
-                .build();
-    }
-    
-    public static Stock getStock(String stockNum) {
-        return new Stock.Builder().stockNum(marketIDGenerate.generateId())
-                .stockNum(stockNum)
-                .build();
-    }
-    
-    public static Storage getStorage(String storageNum) {
-        return new Storage.Builder().storageNum(marketIDGenerate.generateId())
-                .storageNum(storageNum)
-                .build();
-    }
-    
-    public static Store getStore(String storeNum) {
-        return new Store.Builder().storeNum(marketIDGenerate.generateId())
-                .storeNum(storeNum)
-                .build();
-    }
-    
-    public static StoreOwner getStoreOwner(String storeOwnerID) {
-        return new StoreOwner.Builder().storeOwnerID(marketIDGenerate.generateId())
-                .storeOwnerID(storeOwnerID)
-                .build();
-    }
-    
-    public static Street getStreet (String streetNum) {
-        return new Street.Builder().streetNum(marketIDGenerate.generateId())
-                .streetNum(streetNum)
-                .build();
-    }
-    
-    public static Supplier getSupplier(String supplierID) {
-        return new Supplier.Builder().supplierID(marketIDGenerate.generateId())
-                .supplierID(supplierID)
-                .build();
-    }
+
 }
